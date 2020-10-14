647901908a68c2115b4b7dc510e205b0d07ff725
RebelXXHeart <72827861+RebelXXHeart@users.noreply.github.com>
2020-10-13 22:23:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,18 +5,23 @@ import java.text.DecimalFormat;
 
 public class Main {
     static DecimalFormat dt = new DecimalFormat("0.00");
-    private static final int awardMultiplier = 150; // constant for multiplier to calculate cash prize
+    // constant for multiplier to calculate cash prize
+    private static final int awardMultiplier = 150;
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your name >>"); // prompt a message to enter name
-        String name = sc.nextLine(); // store given name in variable
-
-        System.out.println("Enter your GPA >>"); // prompt a message to enter GPA
-        double gpa = sc.nextDouble(); // store given GPA in variable
-
-        computeAward(name, gpa); // passes name and gpa to computeAward
+        // prompt a message to enter name
+        System.out.println("Enter your name >>");
+        // store given name in variable
+        String name = sc.nextLine();
+        
+        // prompt a message to enter GPA
+        System.out.println("Enter your GPA >>");
+        // store given GPA in variable
+        double gpa = sc.nextDouble(); 
+        
+        // passes name and gpa to computeAward
+        computeAward(name, gpa);
 
         sc.close();
     }
@@ -24,8 +29,10 @@ public class Main {
     public static void computeAward(String name, double gpa)
     {
         double userAward;
-        userAward=awardMultiplier * gpa; // calculate the award.
-        System.out.println( name + ", since your gpa is " + gpa + ", NRCC will give you an award of:  $" + dt.format(userAward)); // display the student's name, gpa and award
+        // calculate the award.
+        userAward=awardMultiplier * gpa;
+        // display the student's name, gpa and award
+        System.out.println( name + ", since your gpa is " + gpa + ", NRCC will give you an award of:  $" + dt.format(userAward));
 
     }
 
